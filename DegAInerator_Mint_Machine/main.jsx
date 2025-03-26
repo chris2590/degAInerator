be63716c897dd5ca908fb119597e8c323b352ff5
chris2590 <99231439+chris2590@users.noreply.github.com>
2025-03-25 23:52:33 -0400

    Add files via upload

---
+++ DegAInerator_Mint_Machine/main.jsx
@@ -0,0 +1,5 @@
+// Entry point for Vite React app
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './src/App';
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
---
+++ DegAInerator_Mint_Machine/src/App.jsx
@@ -0,0 +1,3 @@
+import React from 'react';
+const App = () => <div className='text-white text-center p-10'>DegAInerator Mint Machine Coming Soon 💀🔥</div>;
+export default App;
