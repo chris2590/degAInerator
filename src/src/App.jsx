fab540136aa0b002e4f0e5d3c182757d3b317a3f
chris2590 <99231439+chris2590@users.noreply.github.com>
2025-03-28 23:15:22 -0400

    Update App.jsx

--- src/src/App.jsx
+++ src/src/App.jsx
@@ -1,1 +1,10 @@
+import React from 'react';
+
+const App = () => (
+  <div className='text-white text-center p-10 bg-black h-screen'>
+    DegAInerator Mint Machine 💀🔥
+  </div>
+);
+
+export default App;
 
